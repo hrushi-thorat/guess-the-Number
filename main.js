e348703495a23db6eefc6d6610629cac3b400ae9
hrushi-thorat <hrushikeshst54@gmail.com>
2021-06-23 22:36:30 -0700

    Adding js

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 const input = document.getElementById("input");
 const button = document.getElementById("button");
 const result = document.querySelector(".result");
-
+const rulesBtn = document.getElementById("button-rules");
+const rules = document.querySelector(".rules");
+let counter = 0;
 const random = Math.floor(Math.random() * 100) + 1;
 button.addEventListener("click", (e) => {
   e.preventDefault();
+  counter++;
+  console.log(counter);
   console.log(random);
   let user = input.value;
+
   if (user == random) {
     result.innerHTML = "";
-    result.innerHTML = "you Win!";
+    result.innerHTML = `you Win! In ${counter} Moves`;
   } else if (user < random) {
     result.innerHTML = "";
     result.innerHTML = "Your Guess is tooo low";
@@ -17,4 +22,8 @@ button.addEventListener("click", (e) => {
     result.innerHTML = "";
     result.innerHTML = "your Guess is too high";
   }
+});
+
+rulesBtn.addEventListener("click", () => {
+  rules.classList.toggle("open");
 });
